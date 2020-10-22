4e680bf3a7a24c5b0903f2464c202ff7c10c0163
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-22 15:40:17 +0200

    setup tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+const { colors } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        600: "#3e61a5", //45%
+        700: "#0c43a5", //35%
+        900: "#011332", //96%
+      },
+      gray: {
+        100: "#f2f2f2", //95%
+        200: "#e6e6e6", //90%
+        300: "#b9b9b9", //73%
+        400: "#a5a5a5", //65%
+        500: "#919191", //57%
+        600: "#676767", //40%
+        700: "#434343", //26%
+        800: "#262626", //15%
+        900: "#1a1a1a", //10%
+      },
+      black: "#080808",
+      white: "#f7f7f7",
+    },
+    extend: {
+      screens: {
+        dark: { raw: "(prefers-color-scheme: dark)" },
+      },
+    },
   },
   variants: {},
   plugins: [],
