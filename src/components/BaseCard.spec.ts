bc5d7b0849ab1d8ce1737209243866b8fd36f0d9
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-22 16:09:10 +0200

    add default slot

--- src/components/BaseCard.spec.ts
+++ src/components/BaseCard.spec.ts
@@ -1,9 +1,7 @@
-import { Base } from '../../tests/utils/core'
-import Component from './BaseCard.vue'
+import { Base } from "../../tests/utils/core"
+import Component from "./BaseCard.vue"
 const base = new Base(Component)
 
-describe('components/BaseCard.vue', () => {
-  it('', async () => {
-    const { getByText } = base.render()
-  })
+describe("components/BaseCard.vue", () => {
+  it("render default slot", () => base.testHasSlot())
 })
