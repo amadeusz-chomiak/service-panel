3925e82a02ced56a7bbbb68bf7079b60062fff70
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-22 15:50:26 +0200

    cleanup boilerplate components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
-import Home from "../views/Home.vue"
-
+import { defineAsyncComponent } from "vue"
+import Login from '@/views/Login.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Main",
+    component: defineAsyncComponent(() => import("@/views/Main.vue")),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "Login",
+    component: Login
   },
 ]
 
---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+//import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
+
+export default defineComponent({
+  name: "Main",
+  components: {},
+})
+</script>
