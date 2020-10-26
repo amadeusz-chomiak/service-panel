634487e6b86860ba88db958e339fb22677bbbb4d
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-26 16:45:28 +0100

    add links

--- src/composable/useDefinitions.spec.ts
+++ src/composable/useDefinitions.spec.ts
@@ -5,61 +5,168 @@ let category: Category
 let service: Service
 
 describe("composable/useDefinitions.ts", () => {
-  describe('basic', () => {
+  describe("basic", () => {
+    const expectedResult = [
+      {
+        name: "category",
+        description: "description",
+        services: [
+          {
+            brand: {
+              color: "#fff",
+              onColor: "black",
+              name: "service",
+              description: "description",
+            },
+            links: [
+              {
+                name: "link",
+                description: "description",
+                href: "href",
+              },
+            ],
+          },
+        ],
+      },
+    ]
+
     beforeEach(() => {
       renderer = new Renderer()
       category = new Category({
         name: "category",
-        description: "description"
+        description: "description",
       })
       service = new Service({
         brand: {
           color: "#fff",
           onColor: "black",
           name: "service",
-          description: "description"
-        }
+          description: "description",
+        },
+        links: new Map([
+          [
+            "link",
+            {
+              description: "description",
+              href: "href",
+            },
+          ],
+        ]),
       })
-    }) 
-    it("renderer returns category with service brand", async () => {
-      renderer.add(category.add(service))
-  
-      expect(renderer.export()).toStrictEqual([{
-        name: "category",
-        description: "description",
-        services: [
-          { 
-            brand: {
-              color: "#fff",
-              onColor: "black",
-              name: "service",
-              description: "description"
-            }
-          }
-        ]
-      }])
     })
-  
+    it("renderer returns category with service", async () => {
+      renderer.add(category.add(service))
+
+      expect(renderer.export()).toStrictEqual(expectedResult)
+    })
+
     it("renderer remove duplicates from categories and services", async () => {
       renderer
         .add(category.add(service).add(service))
         .add(category)
         .add(category.add(service))
-  
-      expect(renderer.export()).toStrictEqual([{
+
+      expect(renderer.export()).toStrictEqual(expectedResult)
+    })
+  })
+  describe("service links", () => {
+    beforeEach(() => {
+      renderer = new Renderer()
+      category = new Category({
         name: "category",
         description: "description",
-        services: [
-          { 
-            brand: {
-              color: "#fff",
-              onColor: "black",
-              name: "service",
-              description: "description"
-            }
-          }
-        ]
-      }])
+      })
+      service = new Service({
+        brand: {
+          color: "#fff",
+          onColor: "black",
+          name: "service",
+          description: "description",
+        },
+        links: new Map([
+          [
+            "link",
+            {
+              description: "description",
+              href: "href",
+            },
+          ],
+        ]),
+      })
+    })
+    it("service allow links extension", async () => {
+      renderer.add(
+        category.add(
+          service.link("added", {
+            description: "description",
+            href: "href",
+          })
+        )
+      )
+      const expectedResult = [
+        {
+          name: "category",
+          description: "description",
+          services: [
+            {
+              brand: {
+                color: "#fff",
+                onColor: "black",
+                name: "service",
+                description: "description",
+              },
+              links: [
+                {
+                  name: "link",
+                  description: "description",
+                  href: "href",
+                },
+                {
+                  name: "added",
+                  description: "description",
+                  href: "href",
+                },
+              ],
+            },
+          ],
+        },
+      ]
+      expect(renderer.export()).toStrictEqual(expectedResult)
+    })
+
+    it("service allow existing links change", async () => {
+      renderer.add(
+        category.add(
+          service.link("link", {
+            description: "change",
+            href: "change",
+          })
+        )
+      )
+      const expectedResult = [
+        {
+          name: "category",
+          description: "description",
+          services: [
+            {
+              brand: {
+                color: "#fff",
+                onColor: "black",
+                name: "service",
+                description: "description",
+              },
+              links: [
+                {
+                  name: "link",
+                  description: "change",
+                  href: "change",
+                },
+              ],
+            },
+          ],
+        },
+      ]
+      expect(renderer.export()).toStrictEqual(expectedResult)
     })
   })
 })
--- src/definitions/definitionGenerators.ts
+++ src/definitions/definitionGenerators.ts
@@ -1,46 +1,75 @@
+interface ServiceOptionLink {
+  description: string
+  href: string
+}
 
-interface ServiceOptions {
+interface ServiceOptions<Links extends string> {
   brand: {
-    color: string,
-    onColor: 'black' | 'white',
-    name: string,
+    color: string
+    onColor: LiteralUnion<"black" | "white">
+    name: string
     description: string
   }
+  links: Map<LiteralUnion<Links>, ServiceOptionLink>
 }
 
-export class Service {
-  constructor(private readonly options: ServiceOptions){}
+export class Service<Links extends string = string> {
+  constructor(private readonly options: ServiceOptions<Links>) {}
+  public readonly id = Symbol()
+
+  link(name: Links, options: Partial<ServiceOptionLink>): this
+  link(name: string, options: ServiceOptionLink): this
+  link(name: LiteralUnion<Links>, options: Partial<ServiceOptionLink>) {
+    const links = this.options.links
+
+    const oldLink = links.get(name)
+    if (oldLink)
+      links.set(name, { ...oldLink, ...options })
+      
+    if (options.description && options.href)
+      links.set(name, options as ServiceOptionLink)
+
+    return this
+  }
 
   export() {
-    return this.options
+    const options = this.options
+    const links = Array.from(options.links.entries()).map(([name, value]) => ({
+      name,
+      ...value,
+    }))
+    return { ...options, links }
   }
 }
 
-interface CategoryOptions{
-  name: string,
+interface CategoryOptions {
+  name: string
   description: string
 }
 
 export class Category {
-  private readonly services = new Set<ReturnType<Service["export"]>>()
+  private readonly services = new Map<Symbol, ReturnType<Service["export"]>>()
   public readonly id = Symbol()
-  constructor(private readonly options: CategoryOptions){}
-  
+  constructor(private readonly options: CategoryOptions) {}
+
   add(instance: Service) {
-    this.services.add(instance.export())
+    this.services.set(instance.id, instance.export())
     return this
   }
 
   export() {
     return {
       ...this.options,
-      services: Array.from(this.services)
+      services: Array.from(this.services.values()),
     }
-  } 
+  }
 }
 
 export class Renderer {
-  private readonly categories= new Map<Symbol, ReturnType<Category["export"]>>()
+  private readonly categories = new Map<
+    Symbol,
+    ReturnType<Category["export"]>
+  >()
 
   add(instance: Category) {
     this.categories.set(instance.id, instance.export())
@@ -51,4 +80,3 @@ export class Renderer {
     return Array.from(this.categories.values())
   }
 }
-
