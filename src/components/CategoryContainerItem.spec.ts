a9df628278e0d5b4b891babd19aa60828090fda9
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-27 16:05:25 +0100

    implement CategoryContainerItem

--- src/components/CategoryContainerItem.spec.ts
+++ src/components/CategoryContainerItem.spec.ts
@@ -1,11 +1,35 @@
 import { Base } from "../../tests/utils/core"
 import Component from "./CategoryContainerItem.vue"
+import { Category } from "@/composable/useDefinitions"
 const base = new Base(Component, {
-  // props: {},
+  props: {
+    category: {
+      name: "name",
+      description: "description",
+      services: [{}, {}]
+    } as Category,
+  },
 })
 
 describe("components/CategoryContainerItem.vue", () => {
-  it("", async () => {
+  it("render category title", () => {
     const wrapper = base.render()
+    const header = wrapper.get("base-text")
+    expect(header.text()).toContain("name")
+  })
+  it("render category description", () => {
+    const wrapper = base.render()
+    const header = wrapper.get("base-text")
+    expect(header.text()).toContain("description")
+  })
+  it("render service cards", () => {
+    const wrapper = base.render()
+    //? search for two opening tags for category-container-item component
+    expect(wrapper.findAll("service-card-stub")?.length).toBe(2)
+  })
+
+  it("pass 'service' prop to service-card component", async () => {
+    const wrapper = base.render()
+    expect(wrapper.findAll('service-card-stub')[0].attributes().service).toBeTruthy()
   })
 })
--- src/components/CategoryContainerItem.vue
+++ src/components/CategoryContainerItem.vue
@@ -1,12 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <base-text tag="h2" size="2xl">
+      <span>{{ category.name }}&nbsp;</span>
+      <span class="text-xl">{{ category.description }}</span>
+    </base-text>
+    <div>
+      <service-card
+        v-for="(service, index) in category.services"
+        :key="index"
+        :service="service"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from 'vue'
-
-export default defineComponent ({
-  // props: {},
+import { ref, reactive, defineComponent } from "vue"
+import { Category } from "@/composable/useDefinitions"
+import ServiceCard from "./ServiceCard.vue"
+export default defineComponent({
+  components: {
+    ServiceCard,
+  },
+  props: {
+    category: {
+      required: true,
+      type: Object as () => Category,
+    },
+  },
   setup() {
     return {}
   },
