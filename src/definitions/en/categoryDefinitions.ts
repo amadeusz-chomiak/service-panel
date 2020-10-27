dc84969d530b26f16d1de0a54ba4a0146e8b7612
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-27 11:42:58 +0100

    add basic categories and services to english lang

---
+++ src/definitions/en/categoryDefinitions.ts
@@ -0,0 +1,33 @@
+
+import { Category } from '../definitionGenerators'
+
+export const analytic = new Category({
+  name: 'analytic',
+  description: 'for testing Your content'
+})
+
+export const domain = new Category({
+  name: 'domain',
+  description: 'check Your domains status'
+})
+
+export const server = new Category({
+  name: 'server',
+  description: 'check were all site data lives'
+})
+
+//todo implement with payments module
+// export const payments = new Category({
+//   name: 'payments',
+//   description: 'check Your spendings'
+// })
+
+export const development = new Category({
+  name: 'development',
+  description: 'when You need to expand Your site'
+})
+
+export const contentEditor = new Category({
+  name: 'content editor',
+  description: 'for changing this site content'
+})
---
+++ src/definitions/en/serviceDefinitions.ts
@@ -0,0 +1,77 @@
+import { Service } from "../definitionGenerators"
+
+export const firebase = new Service({
+  brand: {
+    name: "firebase",
+    description: "Server provider that combines site hosting, database, authentication, analytics and more",
+    color: "#FFA000",
+    onColor: "white",
+  },
+  links: new Map([
+    [
+      "dashboard",
+      {
+        description: "You'll find there all parts firebase",
+        href: "https://console.firebase.google.com/"
+      },
+    ],
+    [
+      "pricing",
+      {
+        description: "You'll find detailed pricing",
+        href: "https://firebase.google.com/pricing"
+      },
+    ],
+  ]),
+})
+
+export const plausible = new Service({
+  brand: {
+    name: "plausible",
+    description: "Simple and privacy-friendly alternative to Google Analytics",
+    color: "#5850ec",
+    onColor: "white",
+  },
+  links: new Map([
+    [
+      "dashboard",
+      {
+        description: "You'll find there analytics summary",
+        href: "" //* specify Your href to show it
+      },
+    ],
+    [
+      "pricing",
+      {
+        description: "You'll find detailed pricing",
+        href: "https://plausible.io/#pricing"
+      },
+    ],
+  ]),
+})
+
+export const sanity = new Service({
+  brand: {
+    name: "sanity",
+    description: "Sanity is the ultimate content platform that helps teams dream big and deliver quickly",
+    color: "#f03e2f",
+    onColor: "white",
+  },
+  links: new Map([
+    [
+      "dashboard",
+      {
+        description: "You can change site content from here",
+        href: "" //* specify Your href to show it
+      },
+    ],
+    [
+      "pricing",
+      {
+        description: "You'll find detailed pricing",
+        href: "https://www.sanity.io/pricing/compare"
+      },
+    ],
+  ]),
+})
+
