d75380e392759168325563b0bb5ee104e1d53641
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-28 15:40:35 +0100

    add color style generation  for tailwind opacity classes

--- src/composable/useColor.spec.ts
+++ src/composable/useColor.spec.ts
@@ -1,25 +1,39 @@
 import { useColor } from "./useColor"
 //@ts-expect-error
 import Tailwind from "../../tailwind.config.js"
-const tailwindColors =  Tailwind.theme.colors
+const tailwindColors = Tailwind.theme.colors
 describe("composable/useColor.ts", () => {
   it("return tailwind black for black color", () => {
-    const { color } = useColor('black')
+    const { color } = useColor("black")
     expect(color.value).toBe(tailwindColors.black)
   })
 
   it("return tailwind white for white color", () => {
-    const { color } = useColor('white')
+    const { color } = useColor("white")
     expect(color.value).toBe(tailwindColors.white)
   })
 
   it("return color if it's in HEX format", () => {
-    const { color } = useColor('#fff')
-    expect(color.value).toBe('#fff')
+    const { color } = useColor("#fff")
+    expect(color.value).toBe("#fff")
   })
 
   it("throw on non specific and non HEX color", () => {
-    const { color } = useColor('fff')
+    const { color } = useColor("fff")
     expect(() => color.value).toThrow()
+  })
+
+  it("convert hex to rgba background color style", () => {
+    const { backgroundStyle } = useColor("#000")
+    expect(backgroundStyle.value).toBe(
+      "background-color: rgb(0%,0%,0%);background-color: rgba(0%,0%,0%, var(--bg-opacity));"
+    )
+  }) 
+
+  it("convert hex to rgba content color style", () => {
+    const { contentStyle } = useColor("#000")
+    expect(contentStyle.value).toBe(
+      "color: rgb(0%,0%,0%);color: rgba(0%,0%,0%, var(--text-opacity));"
+    )
   })
 })
--- src/composable/useColor.ts
+++ src/composable/useColor.ts
@@ -1,15 +1,52 @@
 import { computed } from "vue"
 //@ts-expect-error
 import Tailwind from "../../tailwind.config.js"
-export type Color = LiteralUnion<'black' | 'white'>
+export type Color = LiteralUnion<"black" | "white">
+
+const hexToRGB = (color: string) => {
+  let r: string | number = 0,
+    g: string | number = 0,
+    b: string | number = 0
+
+  // 3 digits
+  if (color.length == 4) {
+    r = "0x" + color[1] + color[1]
+    g = "0x" + color[2] + color[2]
+    b = "0x" + color[3] + color[3]
+
+    // 6 digits
+  } else if (color.length == 7) {
+    r = "0x" + color[1] + color[2]
+    g = "0x" + color[3] + color[4]
+    b = "0x" + color[5] + color[6]
+  }
+
+  r = +(((r as number) / 255) * 100).toFixed(1)
+  g = +(((g as number) / 255) * 100).toFixed(1)
+  b = +(((b as number) / 255) * 100).toFixed(1)
+
+  return `${r}%,${g}%,${b}%`
+}
+
 export const useColor = (color: Color) => {
-  const tailwindColors =  Tailwind.theme.colors
+  const tailwindColors = Tailwind.theme.colors
   return {
     color: computed(() => {
-      if(color === 'black') return tailwindColors.black
-      if (color === 'white') return tailwindColors.white
-      if(color[0] !== '#') throw new Error('color name has to be in HEX format starting from "#" symbol')
+      if (color === "black") return tailwindColors.black
+      if (color === "white") return tailwindColors.white
+      if (color[0] !== "#")
+        throw new Error(
+          'color name has to be in HEX format starting from "#" symbol'
+        )
       return color
-    })
+    }),
+    backgroundStyle: computed(() => {
+      const rgb = hexToRGB(color)
+      return `background-color: rgb(${rgb});background-color: rgba(${rgb}, var(--bg-opacity));`
+    }),
+    contentStyle: computed(() => {
+      const rgb = hexToRGB(color)
+      return `color: rgb(${rgb});color: rgba(${rgb}, var(--text-opacity));`
+    }),
   }
 }
