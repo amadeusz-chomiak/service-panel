2b79372a49977e999a5fee2e4c436e5a96940ecf
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-11-09 14:48:18 +0100

    hygen MenuContainer

---
+++ src/components/MenuContainer.spec.ts
@@ -0,0 +1,11 @@
+import { Base } from "../../tests/utils/core"
+import Component from "./MenuContainer.vue"
+const base = new Base(Component, {
+  // props: {},
+})
+
+describe("components/MenuContainer.vue", () => {
+  it("", async () => {
+    const wrapper = base.render()
+  })
+})
