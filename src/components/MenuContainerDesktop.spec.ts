45f1e42e2d85265f1aced3d2e7b4973bea45b45c
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-11-12 14:11:10 +0100

    hygen MenuContainer mobile and desktop versions

---
+++ src/components/MenuContainerDesktop.spec.ts
@@ -0,0 +1,11 @@
+import { Base } from "../../tests/utils/core"
+import Component from "./MenuContainerDesktop.vue"
+const base = new Base(Component, {
+  // props: {},
+})
+
+describe("components/MenuContainerDesktop.vue", () => {
+  it("", async () => {
+    const wrapper = base.render()
+  })
+})
---
+++ src/components/MenuContainerMobile.spec.ts
@@ -0,0 +1,11 @@
+import { Base } from "../../tests/utils/core"
+import Component from "./MenuContainerMobile.vue"
+const base = new Base(Component, {
+  // props: {},
+})
+
+describe("components/MenuContainerMobile.vue", () => {
+  it("", async () => {
+    const wrapper = base.render()
+  })
+})
