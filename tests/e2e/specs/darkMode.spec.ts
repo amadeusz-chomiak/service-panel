71c708493cd9199fb34f2a3fea7bc5eb204ceac3
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-11-12 14:09:01 +0100

    add initial dark mode e2e test

--- tests/e2e/specs/darkMode.spec.ts
+++ tests/e2e/specs/darkMode.spec.ts
@@ -1,10 +1,15 @@
+import waitFor from "wait-for-expect"
 
-describe('dark mode', () => {
-  beforeAll(() => {
-    page.goto('http://localhost:8080/')  
+describe("dark mode", () => {
+  beforeAll(async () => {
+    await page.emulateMediaFeatures([
+      { name: "prefers-color-scheme", value: "dark" },
+    ])
+    await page.goto("http://localhost:8080/")
   })
 
-  test('set dark mode by default if prefers-color-scheme is set to dark', async () => {
-    await page.emulateMediaFeatures([{name: 'prefers-color-scheme', value: 'dark'}])
+  test("set dark mode by default if prefers-color-scheme is set to dark", async () => {
+    await page.waitForSelector('main')
+    await page.evaluate(()=> expect(document.documentElement).toHaveClass('dark'))
   })
 })
