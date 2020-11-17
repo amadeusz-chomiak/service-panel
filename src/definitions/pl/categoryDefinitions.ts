f2af7673d858d3edb4528d49f7e7545d58cbde37
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-11-17 15:59:19 +0100

    hygen pl definitions

---
+++ src/definitions/pl/categoryDefinitions.ts
@@ -0,0 +1,26 @@
+import { Category } from '../definitionGenerator'
+
+export const analytic = new Category({
+  name: '',
+  description: ''
+})
+
+export const domain = new Category({
+  name: '',
+  description: ''
+})
+
+export const server = new Category({
+  name: '',
+  description: ''
+})
+
+export const development = new Category({
+  name: '',
+  description: ''
+})
+
+export const contentEditor = new Category({
+  name: '',
+  description: ''
+})
---
+++ src/definitions/pl/serviceDefinitions.ts
@@ -0,0 +1,74 @@
+import * as service from "../global/serviceDefinitions"
+import { Price } from "../definitionGeneratorPrice"
+
+const price = new Price({
+  cost: {
+    free: "",
+    paid: "",
+    flexible: "",
+  },
+  renew: {
+    never: "",
+    onDemand: "",
+    daily: "",
+    weekly: "",
+    monthly: "",
+    quarterly: "",
+    yearly: "",
+  },
+  compose({ renew, cost }, originalPrice) {
+    if (renew) {
+      return `${renew} ${cost}`
+    }
+    return cost
+  },
+})
+
+export const firebase = service
+  .Firebase({
+    brand: {
+      description:
+        "",
+    },
+    price: {
+      localize: price,
+    },
+  })
+  .link("initialize", "dashboard", {
+    title: "",
+    description: "",
+  })
+  .link("initialize", "pricing", {
+    title: "",
+    description: "",
+  })
+
+export const plausible = service
+  .Plausible({
+    brand: {
+      description:
+        "",
+    },
+    price: {
+      localize: price,
+    },
+  })
+  .link("initialize", "pricing", {
+    title: "",
+    description: "",
+  })
+
+export const sanity = service
+  .Sanity({
+    brand: {
+      description:
+        "",
+    },
+    price: {
+      localize: price,
+    },
+  })
+  .link("initialize", "pricing", {
+    title: "",
+    description: "",
+  })
