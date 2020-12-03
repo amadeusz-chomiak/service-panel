58b41905400cbc3a1da2fa6bd006c0b41effa9eb
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-12-03 14:19:25 +0100

    hygen ButtonNewVersion

---
+++ src/components/ButtonNewVersion.spec.ts
@@ -0,0 +1,11 @@
+import { Base } from "../../tests/utils/core"
+import Component from "./ButtonNewVersion.vue"
+const base = new Base(Component, {
+  // props: {},
+})
+
+describe("components/ButtonNewVersion.vue", () => {
+  it("", async () => {
+    const wrapper = base.render()
+  })
+})
