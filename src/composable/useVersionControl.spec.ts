c8b7db4fd66bb5ad897e90b37a0619bde79bc96e
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-12-04 12:38:37 +0100

    implement useVersionControl and move to workbox-window

--- src/composable/useVersionControl.spec.ts
+++ src/composable/useVersionControl.spec.ts
@@ -1,7 +1,17 @@
 import { useVersionControl } from "./useVersionControl"
 
 describe("composable/useVersionControl.ts", () => {
-  it("", () => {
-    const {} = useVersionControl()
+  it("serviceWorkerWaiting is true after setting skip waiting", () => {
+    const { setSkipWaiting, serviceWorkerWaiting } = useVersionControl()
+    setSkipWaiting(async () => { })
+    expect(serviceWorkerWaiting.value).toBe(true)
+  })
+
+  it("call skipWaiting set by setSkipWaiting", () => {
+    const { setSkipWaiting, skipWaiting } = useVersionControl()
+    const fn = jest.fn()
+    setSkipWaiting(fn)
+    skipWaiting.value?.()
+    expect(fn).toBeCalledTimes(1)
   })
 })
--- src/composable/useVersionControl.ts
+++ src/composable/useVersionControl.ts
@@ -1,6 +1,17 @@
-import { computed } from "vue"
+import { computed, ref, readonly } from "vue"
 
+type SkipWaiting = () => Promise<void>
+const skipWaiting = ref<SkipWaiting>()
+ 
 export const useVersionControl = () => {
-  
-  return {}
+  const setSkipWaiting = (to?: SkipWaiting) => {
+    skipWaiting.value = to
+  }
+  const serviceWorkerWaiting = computed(()=> !!skipWaiting.value)
+
+  return {
+    setSkipWaiting,
+    skipWaiting,
+    serviceWorkerWaiting: serviceWorkerWaiting,
+  }
 }
