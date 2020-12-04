eb7c1a07916a8faaec72922cc0ee4d0667c8eca1
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-12-04 13:02:35 +0100

    hygen TransitionFade

---
+++ src/components/TransitionFade.spec.ts
@@ -0,0 +1,11 @@
+import { Base } from "../../tests/utils/core"
+import Component from "./TransitionFade.vue"
+const base = new Base(Component, {
+  // props: {},
+})
+
+describe("components/TransitionFade.vue", () => {
+  it("", async () => {
+    const wrapper = base.render()
+  })
+})
