fcc1c6bcd206e334b02f3547d0f64931374c234f
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-12-07 16:50:43 +0100

    implement ButtonNewVersion

--- src/components/ButtonNewVersion.spec.ts
+++ src/components/ButtonNewVersion.spec.ts
@@ -1,11 +1,40 @@
 import { Base } from "../../tests/utils/core"
 import Component from "./ButtonNewVersion.vue"
+import { useVersionControl } from '@/composable/useVersionControl'
 const base = new Base(Component, {
   // props: {},
 })
 
 describe("components/ButtonNewVersion.vue", () => {
-  it("", async () => {
+  it("set base-tooltip right prop to false by default", async () => {
     const wrapper = base.render()
+
+    const tooltip = wrapper.get("base-tooltip")
+    expect(tooltip.attributes("right")).toBe("false")
+  })
+
+  it("pass tooltip-right prop as base-tooltip right prop", async () => {
+    const wrapper = base.render({
+      props: {
+        tooltipRight: true,
+      },
+    })
+
+    const tooltip = wrapper.get("base-tooltip")
+    expect(tooltip.attributes("right")).toBe("true")
+  })
+
+  it("can be clicked only once", async () => {
+    const wrapper = base.render()
+    const button = wrapper.get("button")
+    const {setSkipWaiting} = useVersionControl()
+    const fn = jest.fn()
+    setSkipWaiting(fn)
+
+    await button.trigger('click')
+    await button.trigger('click')
+    await button.trigger('click')
+    
+    expect(fn).toBeCalledTimes(1)
   })
 })
--- src/components/ButtonNewVersion.vue
+++ src/components/ButtonNewVersion.vue
@@ -1,14 +1,49 @@
 <template>
-  <div></div>
+  <base-tooltip
+    :show="showTooltip"
+    :right="tooltipRight"
+    text="Switch to new version"
+    :timeout="5000"
+    @close="showTooltip = false"
+  >
+    <button
+      :disabled="skipping"
+      class="button button-primary p-3 sm:p-4 overflow-hidden"
+      @click.once="skipWaiting"
+    >
+      <base-icon
+        class="h-5 text-white"
+        :class="{ 'animate-elevator': skipping }"
+        :icon="skipping ? 'downloading' : 'download'"
+      />
+    </button>
+  </base-tooltip>
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from 'vue'
-
-export default defineComponent ({
-  // props: {},
+import { ref, reactive, defineComponent, computed } from "vue"
+import { useVersionControl } from "@/composable/useVersionControl"
+export default defineComponent({
+  props: {
+    tooltipRight: {
+      type: Boolean,
+      default: false,
+    },
+  },
   setup() {
-    return {}
+    const { skipWaiting: skip, skipping } = useVersionControl()
+    const showTooltip = ref(!skipping.value)
+
+    const skipWaiting = () => {
+      showTooltip.value = false
+      skip()
+    }
+
+    return {
+      skipWaiting,
+      showTooltip,
+      skipping,
+    }
   },
 })
 </script>
