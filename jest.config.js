790ae8812df09a2299300912ce26ada1461850e4
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2021-04-07 16:19:28 +0200

    test scoped slot in ButtonNewVersion

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  // todo change main path
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
     "^.+\\.js$": "babel-jest",
     ".*\\.(vue)$": "vue-jest",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testMatch: ["**/src/**/?(*.)+(spec).ts"],
+  testMatch: ["**/src/**/?(*.)+(spec).(ts|tsx)"],
 }
