8bb0cb90de9463abcf8813a8846df1d8e86c4f8c
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2021-06-01 11:34:41 +0200

    Add space between category title and description Fixes #35

--- src/components/CategoryContainerItem.vue
+++ src/components/CategoryContainerItem.vue
@@ -4,12 +4,10 @@
       :id="category.name"
       class="text-4xl leading-none text-black dark:text-gray-200 pt-16 md:pt-32 lg:pt-36 xl:pt-38 2xl:pt-40 mb-4 "
     >
-      <span class="first-letter:uppercase font-semibold inline-block">{{
-        category.name
-      }}</span>
-      <span class="text-2xl pl-1 md:pl-2 inline-block">{{
-        category.description
-      }}</span>
+      <span class="first-letter:uppercase font-semibold inline-block"
+        >{{ category.name }}&nbsp;
+      </span>
+      <span class="text-2xl inline-block"> {{ category.description }}</span>
     </h2>
     <div
       class="flex flex-col space-y-8 2xl:space-y-0 2xl:grid grid-cols-2 gap-8"
